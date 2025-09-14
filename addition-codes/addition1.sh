1f4170405d79f5fcbdeaa398aabfae46ef35f912
chpk228 <pk7057764@gmail.com>
2025-09-14 16:35:42 +0530

    Update addition1.sh
    comments are removed

--- addition-codes/addition1.sh
+++ addition-codes/addition1.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Take two numbers as input
+
 echo "Enter the first number:"
 read num1
 
 echo "Enter the second number:"
 read num2
 
-# Perform addition
+
 sum=$((num1 + num2))
 
-# Display the result
+
 echo "Sum: $sum"
 
